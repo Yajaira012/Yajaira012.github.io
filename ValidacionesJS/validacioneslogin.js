785ca028479a1395c65353afad289e961f1240fd
Yajaira012 <157139503+Yajaira012@users.noreply.github.com>
2024-01-22 19:33:10 -0600

    Add files via upload

---
+++ ValidacionesJS/validacioneslogin.js
@@ -0,0 +1,32 @@
+// script.js
+
+var sanitizedEmail;
+var sanitizedPassword;
+
+function validateLogin() {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    if (email === '' || password === '') {
+        alert('Por favor, complete todos los campos.');
+        return;
+    }
+
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        alert('Ingrese un correo electrónico válido.');
+        return;
+    }
+
+    sanitizedEmail = email.replace(/[^\w\s.@]/gi, '');
+    sanitizedPassword = password.replace(/[^\w\s]/gi, '');
+
+    console.log('Correo electrónico sanitizado:', sanitizedEmail);
+    console.log('Contraseña sanitizada:', sanitizedPassword);
+
+    if (sanitizedEmail === '20230@virtual.utsc.edu.mx' && sanitizedPassword === 'DesarrolloProfesional') {
+        window.location.href = 'Seccion/menu.html';
+    } else {
+        alert('Credenciales incorrectas. Inténtelo de nuevo.');
+    }
+}
